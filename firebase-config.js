17cd0335feb5aa40d8600f6537d5fed0a5a5b64d
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-08-17 13:04:29 +0530

    Add Firebase Analytics to configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,19 @@
-// Import Firebase SDK Modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 
-// Apne Firebase Console se Config Details Yahan Replace Karein
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBYy7OfmGGfnRllOcRvgjtClr0T845kqAA",
+  authDomain: "pinaki-1fe56.firebaseapp.com",
+  projectId: "pinaki-1fe56",
+  storageBucket: "pinaki-1fe56.firebasestorage.app",
+  messagingSenderId: "779330744487",
+  appId: "1:779330744487:web:39f316a974b3f48b692b82",
+  measurementId: "G-LW63K3V555"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
