f8bde15bc5219b36fa5f4988b6171cbd805a53f3
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-08-17 13:22:16 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBYy7OfmGGfnRllOcRvgjtClr0T845kqAA",
   authDomain: "pinaki-1fe56.firebaseapp.com",
+  databaseURL: "https://pinaki-1fe56-default-rtdb.firebaseio.com",
   projectId: "pinaki-1fe56",
   storageBucket: "pinaki-1fe56.firebasestorage.app",
   messagingSenderId: "779330744487",
@@ -16,4 +18,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const database = getDatabase(app);
 export const analytics = getAnalytics(app);
